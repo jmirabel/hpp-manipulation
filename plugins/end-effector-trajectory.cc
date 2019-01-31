c682a5849d3382f36d7f3d401e8b27f26ba835fb
Joseph Mirabel <jmirabel@laas.fr>
2019-01-31 15:41:19 +0100

    Add EndEffectorTrajectory steering method + plugin.

---
+++ plugins/end-effector-trajectory.cc
@@ -0,0 +1,121 @@
+// Copyright (c) 2019, Joseph Mirabel
+// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+//
+// This file is part of hpp-manipulation.
+// hpp-manipulation is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-manipulation is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-manipulation. If not, see <http://www.gnu.org/licenses/>.
+
+#include <hpp/constraints/implicit.hh>
+#include <hpp/constraints/differentiable-function.hh>
+
+#include <hpp/core/path-vector.hh>
+#include <hpp/core/plugin.hh>
+#include <hpp/core/problem.hh>
+#include <hpp/core/problem-solver.hh>
+#include <hpp/core/config-projector.hh>
+
+#include <hpp/manipulation/steering-method/end-effector-trajectory.hh>
+
+namespace hpp {
+  namespace manipulation {
+    using core::Parameter;
+    using steeringMethod::EndEffectorTrajectory;
+    using steeringMethod::EndEffectorTrajectoryPtr_t;
+
+    class SetEndEffectorTrajectory : public core::Command
+    {
+      public:
+        SetEndEffectorTrajectory ()
+          : Command ("Setup EndEffectorTrajectory steering method.\n"
+              "Arguments are:\n"
+              "- constraint name\n"
+              "- path index\n")
+        {
+          valueTypes (core::list_of
+              (Parameter::STRING)
+              (Parameter::INT)
+              (Parameter::FLOAT)
+              (Parameter::FLOAT)
+              (Parameter::BOOL));
+        }
+
+      protected:
+        Parameter doExecute (core::ProblemSolverPtr_t ps)
+        {
+          std::string cname = getParameterValues()[0].stringValue();
+          size_type iPath   = getParameterValues()[1].intValue();
+          value_type start  = getParameterValues()[2].floatValue();
+          value_type end    = getParameterValues()[3].floatValue();
+          bool applyLog     = getParameterValues()[4].boolValue();
+
+#if 1
+          constraints::ImplicitPtr_t constraint = ps->numericalConstraint (cname);
+          if (!constraint)
+            throw std::invalid_argument ("Constraint not found.");
+#else
+          core::ConstraintSetPtr_t c (ps->constraints());
+          if (!c || !c->configProjector())
+            throw std::logic_error ("Problem constraint must have a ConfigProjector");
+          ConfigProjectorPtr_t cp ();
+
+          constraints::ImplicitPtr_t constraint;
+          const core::NumericalConstraints_t& ncs = c->configProjector()->numericalConstraints();
+          for (std::size_t i = 0; i < ncs.size(); ++i) {
+            if (ncs[i]->function().name() == cname) {
+              constraint = ncs[i];
+              break;
+            }
+          }
+#endif
+
+          if (iPath < 0 || (std::size_t)iPath >= ps->paths().size())
+            throw std::invalid_argument ("Path index out of range");
+          core::PathPtr_t path = ps->paths()[iPath];
+          core::interval_t tr = path->timeRange ();
+          if (tr.first != start || tr.second != end) {
+            tr = core::interval_t(start,end);
+            path = path->extract (tr);
+          }
+
+          EndEffectorTrajectoryPtr_t sm = HPP_DYNAMIC_PTR_CAST (EndEffectorTrajectory,
+              ps->problem()->steeringMethod ());
+          if (!sm)
+            throw std::invalid_argument ("Steering method is not of type EndEffectorTrajectory");
+
+          sm->trajectoryConstraint (constraint->copy());
+          sm->trajectory (path, applyLog);
+
+          return Parameter();
+        }
+    };
+
+    class EndEffectorTrajectoryPlugin : public core::ProblemSolverPlugin
+    {
+      public:
+        EndEffectorTrajectoryPlugin ()
+          : ProblemSolverPlugin ("EndEffectorTrajectoryPlugin", "0.0")
+        {}
+
+      protected:
+        virtual bool impl_initialize (core::ProblemSolverPtr_t ps)
+        {
+          ps->steeringMethods.add ("EndEffectorTrajectory", EndEffectorTrajectory::create);
+          ps->commands.add ("setupEndEffectorTrajectory",
+              core::CommandPtr_t (new SetEndEffectorTrajectory()));
+
+          return true;
+        }
+    };
+  } // namespace manipulation
+} // namespace hpp
+
+HPP_CORE_DEFINE_PLUGIN(hpp::manipulation::EndEffectorTrajectoryPlugin)
---
+++ src/steering-method/end-effector-trajectory.cc
@@ -0,0 +1,297 @@
+// Copyright (c) 2019, LAAS-CNRS
+// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+//
+// This file is part of hpp-manipulation.
+// hpp-manipulation is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-manipulation is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-manipulation. If not, see <http://www.gnu.org/licenses/>.
+
+#include <hpp/manipulation/steering-method/end-effector-trajectory.hh>
+
+#include <hpp/util/indent.hh>
+
+#include <hpp/pinocchio/util.hh>
+#include <hpp/pinocchio/device.hh>
+#include <hpp/pinocchio/urdf/util.hh>
+
+#include <hpp/constraints/differentiable-function.hh>
+#include <hpp/constraints/implicit.hh>
+
+#include <hpp/core/config-projector.hh>
+#include <hpp/core/path.hh>
+#include <hpp/core/problem.hh>
+#include <hpp/core/straight-path.hh>
+
+namespace hpp {
+  namespace manipulation {
+    namespace steeringMethod {
+      namespace {
+        /// Class only for testing purpose.
+        class Interpolation : public constraints::DifferentiableFunction
+        {
+          public:
+            Interpolation (const matrix_t& points,
+                const vector_t& times)
+              : DifferentiableFunction (1, 1, points.cols()),
+              points_ (points),
+              times_ (times)
+            {}
+
+          protected:
+            void impl_compute (core::LiegroupElementRef result, vectorIn_t arg) const
+            {
+              value_type t = arg[0];
+              size_type i = rank (t);
+              value_type u = (t - times_[i]) / (times_[i+1] - times_[i]);
+              result.vector() = (1-u) * points_.row(i) + u * points_.row(i+1);
+            }
+
+            void impl_jacobian (matrixOut_t jacobian, vectorIn_t arg) const
+            {
+              value_type t = arg[0];
+              size_type i = rank (t);
+              jacobian = points_.row(i+1) - points_.row(i);
+            }
+
+            std::ostream& print (std::ostream& os) const
+            {
+              return os << "Interpolation: " << one_line (times_);
+            }
+
+          private:
+            size_type rank (value_type t) const
+            {
+              for (size_type i = 1; i < times_.size(); ++i)
+                if (t < times_[i]) return i-1; // between i-1 and i
+              return times_.size() - 2; // between i-2 and i-1
+            }
+
+            matrix_t points_;
+            vector_t times_;
+        };
+
+        template <bool ApplyLog6>
+        class FunctionFromPath : public constraints::DifferentiableFunction
+        {
+          public:
+            FunctionFromPath (const PathPtr_t& p)
+              : DifferentiableFunction (1, 1, ApplyLog6 ? 6 : p->outputSize()),
+              path_ (p)
+            {
+              assert (!ApplyLog6 ||
+                  (p->outputSize() == 7 && p->outputDerivativeSize() == 6));
+            }
+
+            const PathPtr_t& path () const
+            {
+              return path_;
+            }
+
+            std::ostream& print (std::ostream& os) const
+            {
+              return os
+                << (ApplyLog6 ? "FunctionFromPathLog6: " : "FunctionFromPath: ")
+                << path_->timeRange().first << ", "
+                << path_->timeRange().second
+                ;
+            }
+
+          protected:
+            void impl_compute (core::LiegroupElementRef result, vectorIn_t arg) const
+            {
+              bool success;
+              if (ApplyLog6) {
+                Transform3f t;
+                success = compute (arg[0], t);
+                //result.vector() = ::pinocchio::log6 (t).toVector();
+                assert (result.vector().size() == 6);
+                result.vector().head<3>() = t.translation();
+                value_type theta;
+                result.vector().tail<3>() = ::pinocchio::log3 (t.rotation(), theta);
+              } else {
+                success = (*path_) (result.vector(), arg[0]);
+              }
+              if (!success) {
+                hppDout (warning, "Failed to evaluate path at param " << arg[0]
+                    << incindent << iendl << *path_ << decindent);
+              }
+            }
+
+            void impl_jacobian (matrixOut_t jacobian, vectorIn_t arg) const
+            {
+              if (ApplyLog6) {
+                Eigen::Matrix<value_type, 6, 1> der;
+                Eigen::Matrix<value_type, 6, 6> Jlog;
+                Transform3f t;
+
+                compute (arg[0], t);
+                path_->derivative (der, arg[0], 1);
+
+                ::pinocchio::Jlog6 (t, Jlog);
+
+                jacobian.noalias() = Jlog * der;
+              } else {
+                path_->derivative (jacobian.col(0), arg[0], 1);
+              }
+            }
+
+            bool compute (const value_type& time, Transform3f& t) const
+            {
+              Eigen::Matrix<value_type, 7, 1> res;
+              bool success = (*path_) (res, time);
+
+              t = Transform3f (
+                  Eigen::Map<Transform3f::Quaternion> (res.tail<4>().data()).matrix(),
+                  res.head<3>());
+              return success;
+            }
+
+          private:
+            PathPtr_t path_;
+        };
+      }
+
+      EndEffectorTrajectoryPtr_t EndEffectorTrajectory::createWithGuess
+        (const core::Problem& problem)
+      {
+        matrix_t points = problem.getParameter ("EndEffectorTrajectory/points").matrixValue();
+        vector_t times = problem.getParameter ("EndEffectorTrajectory/times").vectorValue();
+        std::string cname = problem.getParameter ("EndEffectorTrajectory/constraint").stringValue();
+
+        core::ConstraintSetPtr_t c (problem.constraints());
+        if (!c || !c->configProjector())
+          throw std::logic_error ("Problem constraint must have a ConfigProjector");
+        ConfigProjectorPtr_t cp (c->configProjector());
+
+        ImplicitPtr_t ic;
+        const core::NumericalConstraints_t& ncs = cp->numericalConstraints();
+        for (std::size_t i = 0; i < ncs.size(); ++i) {
+          if (ncs[i]->function().name() == cname) {
+            ic = ncs[i];
+            break;
+          }
+        }
+        if (!ic)
+          throw std::logic_error ("Constraint not found");
+        if (times[0] != 0)
+          throw std::logic_error ("Times[0] must be 0");
+        for (size_type i = 1; i < times.size(); ++i)
+          if (times[i-1] >= times[i])
+            throw std::logic_error ("Times must be strictly increasing");
+        if (points.rows() != times.size())
+          throw std::logic_error ("Points and times size mismatch.");
+        if (points.cols() != ic->rhsSize())
+          throw std::logic_error ("Points size does not match constraint right hand side.");
+
+        DifferentiableFunctionPtr_t path (new Interpolation (points, times));
+
+        EndEffectorTrajectoryPtr_t sm = create (problem);
+        sm->trajectory (path, interval_t(times[0], times[times.size()-1]));
+        sm->trajectoryConstraint (ic);
+
+        return sm;
+      }
+
+      void EndEffectorTrajectory::trajectoryConstraint (const constraints::ImplicitPtr_t& ic)
+      {
+        constraint_ = ic;
+        if (eeTraj_) trajectory (eeTraj_, timeRange_);
+      }
+
+      void EndEffectorTrajectory::trajectory (const PathPtr_t& eeTraj, bool applyLog6)
+      {
+        if (applyLog6)
+          trajectory (DifferentiableFunctionPtr_t
+              (new FunctionFromPath <true > (eeTraj)), eeTraj->timeRange());
+        else
+          trajectory (DifferentiableFunctionPtr_t
+              (new FunctionFromPath <false> (eeTraj)), eeTraj->timeRange());
+      }
+
+      void EndEffectorTrajectory::trajectory (const DifferentiableFunctionPtr_t& eeTraj, const interval_t& tr)
+      {
+        assert (eeTraj->inputSize() == 1);
+        assert (eeTraj->inputDerivativeSize() == 1);
+        eeTraj_ = eeTraj;
+        timeRange_ = tr;
+
+        if (!constraint_) return;
+
+        constraint_->rightHandSideFunction (eeTraj_);
+      }
+
+      PathPtr_t EndEffectorTrajectory::impl_compute (ConfigurationIn_t q1,
+          ConfigurationIn_t q2) const
+      {
+        try {
+        if (!eeTraj_) throw std::logic_error ("EndEffectorTrajectory not initialized.");
+        // Update (or check) the constraints
+        core::ConstraintSetPtr_t c (constraints());
+        if (!c || !c->configProjector()) {
+          throw std::logic_error ("EndEffectorTrajectory steering method must "
+              "have a ConfigProjector");
+        }
+        ConfigProjectorPtr_t cp (c->configProjector());
+
+        const core::NumericalConstraints_t& ncs = cp->numericalConstraints();
+        bool ok = false;
+        for (std::size_t i = 0; i < ncs.size(); ++i) {
+          if (ncs[i] == constraint_) {
+            ok = true;
+            break; // Same pointer
+          }
+          // Here, we do not check the right hand side on purpose.
+          // if (*ncs[i] == *constraint_) {
+          if (ncs[i]->functionPtr() == constraint_->functionPtr()
+              && ncs[i]->comparisonType() == constraint_->comparisonType()) {
+            ok = true;
+            // TODO We should only modify the path constraint.
+            // However, only the pointers to implicit constraints are copied
+            // while we would like the implicit constraints to be copied as well.
+            ncs[i]->rightHandSideFunction (eeTraj_);
+            break; // logically identical
+          }
+        }
+        if (!ok) {
+          HPP_THROW (std::logic_error, "EndEffectorTrajectory could not find "
+              "constraint " << constraint_->function());
+        }
+
+        return core::StraightPath::create (problem().robot(), q1, q2, timeRange_, c);
+        } catch (const std::exception& e) {
+          std::cout << timeRange_.first << ", " << timeRange_.second << '\n';
+          if (eeTraj_)
+            std::cout << (*eeTraj_) (vector_t::Constant(1,timeRange_.first )) << '\n'
+                      << (*eeTraj_) (vector_t::Constant(1,timeRange_.second)) << std::endl;
+          std::cout << *constraints() << std::endl;
+          std::cout << e.what() << std::endl;
+          throw;
+        }
+      }
+
+      using core::Parameter;
+      using core::ParameterDescription;
+
+      HPP_START_PARAMETER_DECLARATION(EndEffectorTrajectory)
+      core::Problem::declareParameter(ParameterDescription(Parameter::STRING,
+            "EndEffectorTrajectory/constraint",
+            "Name of the constraint in the problem constraint which represent"
+            ));
+      core::Problem::declareParameter(ParameterDescription(Parameter::MATRIX,
+            "EndEffectorTrajectory/points",
+            ""));
+      core::Problem::declareParameter(ParameterDescription(Parameter::VECTOR,
+            "EndEffectorTrajectory/times",
+            ""));
+      HPP_END_PARAMETER_DECLARATION(EndEffectorTrajectory)
+    } // namespace steeringMethod
+  } // namespace manipulation
+} // namespace hpp
